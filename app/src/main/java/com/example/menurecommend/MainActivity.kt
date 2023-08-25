dd4a15ca4cea142b584e769c5c25de34d9e42ef7
Euna-6 <Euna-6>
2023-08-25 20:09:15 +0900

    ### Add MainActivity 에서 BackKey Double Touch 시 종료

--- app/src/main/java/com/example/menurecommend/MainActivity.kt
+++ app/src/main/java/com/example/menurecommend/MainActivity.kt
@@ -3,12 +3,16 @@ package com.example.menurecommend
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import com.example.menurecommend.databinding.ActivityMainBinding
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     val cnt = MenuItem.menuList.count()
+    var backKeyPressedTime : Long = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,5 +33,17 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, ListActivity::class.java)
             startActivity(intent)
         }
+
+
+
+    }
+
+    override fun onBackPressed() {
+        if(System.currentTimeMillis() - 2000 > backKeyPressedTime){
+            Toast.makeText(this, "한 번 더 누르시면 종료됩니다.", Toast.LENGTH_SHORT).show()
+        } else {
+            finish()
+        }
+        backKeyPressedTime = System.currentTimeMillis()
     }
 }
